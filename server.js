93197c34ee0d702a1c52968dec264fa53caad060
jandales <jesusandales021993@gmail.com>
2022-09-12 22:40:13 +0800

    add express

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 const  app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')))
+app.use(express.static(path.join(__dirname, '..', '/dist')))
 
 
 app.get(/.*/, function (req, res){
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 });
 
+console.log(path.join(__dirname, '..', '/dist'));
 const port =  process.env.PORT  || 8081
 
 app.listen(port);
